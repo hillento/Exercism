a51063113bba1b22d170e18e37a7a7868d6c6529
Alex T Brown <brown.alex.tyler@gmail.com>
2019-10-31 12:50:37 -0600

    A work in progress

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,30 @@
+# Currently a work in progress
+Codons = {
+    "AUG" :  "Methionine",
+    "UUU" : "Phenylalanine",
+    "UUC" : "Phenylalanine",
+    "UUA" : "Leucine",
+    "UUG" : "Leucine",
+    "UCU" : "Serine",
+    "UCC" : "Serine",
+    "UCA" : "Serine",
+    "UCG" : "Serine",
+    "UAU" : "Tyrosine",
+    "UAC" : "Tyrosine",
+    "UGU" : "Cysteine",
+    "UGC" : "Cysteine",
+    "UGG" : "Tryptophan",
+    "UAA" : "STOP",
+    "UAG" : "STOP",
+    "UGA" : "STOP"
+
+}
+
 def proteins(strand):
-    pass
+    codonList = [[strand[i:i+3] for i in range(0, len(strand), 3)]]
+    proteinList = []
+    for codon in codonList:
+        if codon in Codons:
+            proteinList.append(Codons[codon])
+    return proteinList
+
