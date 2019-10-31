d3ff0657d34b0a8c8524d07c4d45628c2c70cfac
Alex T Brown <brown.alex.tyler@gmail.com>
2019-10-31 10:26:52 -0600

    Add existing file

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,1 +1,27 @@
 package space
+
+// Planet is a placeholder for which planet the user inputs
+type Planet string
+
+// EarthYear is how many seconds it takes the earth to revolve around the sun once
+const EarthYear = 31557600
+
+// Age sets up the map/dictionary for the planets. It assigns each planet to a decimal of how many seconds its own revolution takes
+func Age(days float64, planet Planet) float64 {
+	PlanetYrs := map[Planet]float64{
+		"Mercury": EarthYear * 0.2408467,
+		"Venus":   EarthYear * 0.61519726,
+		"Earth":   EarthYear,
+		"Mars":    EarthYear * 1.8808158,
+		"Jupiter": EarthYear * 11.862615,
+		"Saturn":  EarthYear * 29.447498,
+		"Uranus":  EarthYear * 84.016846,
+		"Neptune": EarthYear * 164.79132,
+	}
+	return PlanetAgeCalc(PlanetYrs[planet], days)
+}
+
+// PlanetAgeCalc is used to calculate the years on the given planet based on the value passed by the dictionary PlanetYrs
+func PlanetAgeCalc(planetYr float64, seconds float64) float64 {
+	return seconds / planetYr
+}
