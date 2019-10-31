7511dd502a43993f38f0aa487a4bd26ca7e2b778
Alex T Brown <brown.alex.tyler@gmail.com>
2019-10-31 13:33:29 -0600

    Solved Exercise

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,5 +1,5 @@
 # Currently a work in progressgit
-Codons = {
+Codon2Protein = {
     "AUG" :  "Methionine",
     "UUU" : "Phenylalanine",
     "UUC" : "Phenylalanine",
@@ -21,10 +21,18 @@ Codons = {
 }
 
 def proteins(strand):
-    codonList = [[strand[i:i+3] for i in range(0, len(strand), 3)]]
+    # uses slicing to split the string into strings of 3 characters.
+    codonList = [strand[i:i+3] for i in range(0, len(strand), 3)]
+    #initialize an empty list 
     proteinList = []
+    #for each item in the codonList....
     for codon in codonList:
-        if codon in Codons:
-            proteinList.append(Codons[codon])
+        #...and it it translates to stop in the dictionart break -> return proteinList
+        if Codon2Protein[codon] == "STOP" : break
+        #...translate the value of the codon from the dictionary. then append it to the proteinList
+        proteinList.append(Codon2Protein[codon])
     return proteinList
+
+
+
 
