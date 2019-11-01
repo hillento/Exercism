b52ceaa8024dd865c99ebca888314098c22e5480
Alex T Brown <brown.alex.tyler@gmail.com>
2019-10-31 19:08:28 -0600

    Completed 'Hamming' exercise.

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -9,7 +9,19 @@ Remove this comment before submitting your exercise.
 
 function distance(string $strandA, string $strandB) : int
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    $dif = 0;
+   
+    if (strlen($strandA) != strlen($strandB))
+    {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+
+    for ($i = 0; $i < strlen($strandA); $i++)
+    {
+        if ($strandA[$i] != $strandB[$i]){
+            $dif++;
+        }
+    }
+
+    return $dif;
 }
