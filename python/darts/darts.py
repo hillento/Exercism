b4801f698b2b8d35c309881d070d0445953f812f
Alex T Brown <brown.alex.tyler@gmail.com>
2019-11-08 02:13:59 -0700

    Fixed unnecessary calculations in if statement

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -2,13 +2,13 @@ import math
 
 def score(x, y):
     hyp = pythag(x, y)
-    if(hyp > 10 or (abs(x)>10 or abs(y) > 10)):
+    if(hyp > 10):
         return 0
-    elif(hyp > 5 or (abs(x)>5 or abs(y) > 5)):
+    elif(hyp > 5):
         return 1
-    elif(hyp > 1  or (abs(x)>1 or abs(y) > 1)):
+    elif(hyp > 1):
         return 5
-    elif(hyp <= 1):
+    else:
         return 10
 def pythag(x, y):
     return math.sqrt((x*x)+(y*y))
